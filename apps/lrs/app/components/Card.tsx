5de5a1eea4a5ab09d4145b6e7d5a04b54be1d66e
Sofia Calderon <sofia.ester.calderon@gmail.com>
2025-02-26 14:56:16 +1100

    put card back into lrs

---
+++ apps/lrs/app/components/Card.tsx
@@ -0,0 +1,13 @@
+import { storyblokEditable } from "@storyblok/react/rsc";
+
+export const Card = (params: any) => {
+  return (
+    <div
+      {...storyblokEditable(params.blok)}
+      className="bg-white p-8 rounded-sm shadow"
+    >
+      <h3 className="font-bold text-3xl">{params.blok.headline}</h3>
+      <p className="mt-6 text-lg">{params.blok.content}</p>
+    </div>
+  );
+};
--- apps/lrs/app/storyblok.ts
+++ apps/lrs/app/storyblok.ts
@@ -3,8 +3,8 @@ import { Article } from "./components/Article";
 import { Page } from "./components/Page";
 import { Hero } from "./components/Hero";
 import { Grid } from "./components/Grid";
+import { Card } from "./components/Card";
 import { RecommendedArticles } from "./components/RecommendedArticles";
-import { CardComponent } from "@repo/ui/card";
 
 export const getStoryblokApi = storyblokInit({
   accessToken: process.env.NEXT_PUBLIC_STORYBLOK_TOKEN,
@@ -14,7 +14,7 @@ export const getStoryblokApi = storyblokInit({
     page: Page,
     hero: Hero,
     grid: Grid,
-    card: CardComponent,
+    card: Card,
     recommended_articles: RecommendedArticles,
   },
   enableFallbackComponent: true,
